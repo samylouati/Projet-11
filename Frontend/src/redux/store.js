7ce59994395d1f695b1cc7ce3021280ae060a8ef
samylouati <148778977+samylouati@users.noreply.github.com>
2024-08-11 11:20:48 +0200

    config redux

---
+++ Frontend/src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {
+    // Nous allons ajouter les reducers ici plus tard
+  },
+});
+
+export default store;
